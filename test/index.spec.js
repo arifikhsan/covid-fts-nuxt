1352a68a50efb75e2f479c6d2b342402839b1c07
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-07-10 14:03:00 +0700

    Index page

---
+++ test/index.spec.js
@@ -0,0 +1,15 @@
+import { get, setupTest } from "@nuxt/test-utils";
+
+describe("Index page", () => {
+  setupTest({
+    server: true,
+    setupTimeout: 5 * 60 * 1000
+  });
+
+  test("it contain aktif", async () => {
+    const { body } = await get('/')
+
+    expect(body).toContain('aktif')
+  });
+});
+
